2a25c2fd8f0def2684980c2100401010ca68b9b6
Alex Bradner <alex@bradner.net>
2016-11-16 13:51:43 +1000

    Fixing up backslashes in sublime module

--- common/sublime.sh
+++ common/sublime.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-
-SUBL_PATH_OSX=~/Library/Application Support/Sublime Text 3/
-SUBL_PATH_LINUX=~/Library/Application Support/Sublime Text 3/
-
+HOME_DIR=`echo ~`
+SUBL_PATH_OSX="$HOME_DIR/Library/Application Support/Sublime Text 3"
+SUBL_PATH_LINUX="$HOME_DIR/.config/sublime-text-3"
+ 
 if [ "$(uname)" == "Darwin" ]; then
 	SUBL_PATH=$SUBL_PATH_OSX
 else
@@ -11,18 +11,19 @@ fi
 
 echo "Installing Package Control for sublime 3"
 echo "using directory $SUBL_PATH"
+sleep 2
 
-curl -fLo $SUBL_PATH/Installed\ Packages/Package\ Control.sublime-package --create-dirs \
+curl -fLo "$SUBL_PATH/Installed Packages/Package Control.sublime-package" --create-dirs \
     https://packagecontrol.io/Package%20Control.sublime-package
 
 echo "Installing basic sublime config"
-if [ -f $SUBL_PATH/Packages/User/Package\ Control.sublime-settings ]; then
-   mv $SUBL_PATH/Packages/User/Package\ Control.sublime-settings \
-   $SUBL_PATH/Packages/User/Package\ Control.sublime-settings.backup
+if [ -f "$SUBL_PATH/Packages/User/Package Control.sublime-settings" ]; then
+   mv "$SUBL_PATH/Packages/User/Package Control.sublime-settings" \
+   "$SUBL_PATH/Packages/User/Package Control.sublime-settings.backup"
 else
-	mkdir -p $SUBL_PATH/Packages/User	
+	mkdir -p "$SUBL_PATH/Packages/User"
 fi
 
-ln -s `pwd`/Package\ Control.sublime-settings $SUBL_PATH/Packages/User/Package\ Control.sublime-settings
+ln -s "`pwd`/Package Control.sublime-settings" "$SUBL_PATH/Packages/User/Package Control.sublime-settings"
 
 echo "Sublime done"
